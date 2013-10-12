739fabee7370f0c9bd65f7e5fac147644c92d31f
Swaroop SM <swaroop.striker@gmail.com>
2013-10-12 12:37:40 +0530

    new class for command namespace

---
+++ lib/striker/command/new.rb
@@ -0,0 +1,14 @@
+module Striker
+	module Command
+		class New
+
+			# Generates new site and related directories
+			def self.process(args, options)
+				site_name = args.join
+				FileUtils.mkdir site_name
+				FileUtils.cp_r Dir.glob(File.expand_path("../../../new_site", __FILE__) + "/*"), File.join(Dir.pwd, site_name)
+			end
+
+		end
+	end
+end
