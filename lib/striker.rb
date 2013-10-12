04c5c2399b92854405d77a85a83f7993270a7f2a
Swaroop SM <swaroop.striker@gmail.com>
2013-10-12 13:36:23 +0530

    require page command class

--- lib/striker.rb
+++ lib/striker.rb
@@ -2,6 +2,8 @@ require "fileutils"
 
 require_relative "striker/version"
 require_relative "striker/command/new"
+require_relative "striker/command/page"
+require_relative "striker/settings"
 
 module Striker
 
