b1d043c085b176ceb6fbe0c8cf0399deb9dc4365
Swaroop SM <swaroop.striker@gmail.com>
2013-10-13 20:44:52 +0530

    used safe yaml to load page meta info

--- lib/striker/page.rb
+++ lib/striker/page.rb
@@ -1,14 +1,21 @@
+require 'stringex_lite'
+require 'safe_yaml'
+
+SafeYAML::OPTIONS[:default_mode] = :safe
+
 module Striker
 	class Page
-		
-		attr_reader :meta, :content, :title
+
+
+		attr_reader :meta, :content, :title, :name
 
 		def initialize(page)
-			page = Settings::PAGES_DIR + page
+			page = File.join Settings::PAGES_DIR, page
 			@meta = YAML.load_file(page)
 			@title = @meta['title']
 			extract_content = File.open(page, 'r').read.match(/^(?<headers>---\s*\n.*?\n?)^(---\s*$\n?)/m)
 			@content = extract_content.post_match
+			@name = @meta['title'].to_url
 		end
 
 	end
