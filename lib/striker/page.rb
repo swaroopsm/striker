b6e3a823ad9ad239e1f94d8ba8d82b9f0b470cbf
Swaroop SM <swaroop.striker@gmail.com>
2013-10-13 19:47:44 +0530

    added title getter method

--- lib/striker/page.rb
+++ lib/striker/page.rb
@@ -1,11 +1,12 @@
 module Striker
 	class Page
 		
-		attr_reader :meta, :content
+		attr_reader :meta, :content, :title
 
 		def initialize(page)
 			page = Settings::PAGES_DIR + page
 			@meta = YAML.load_file(page)
+			@title = @meta['title']
 			extract_content = File.open(page, 'r').read.match(/^(?<headers>---\s*\n.*?\n?)^(---\s*$\n?)/m)
 			@content = extract_content.post_match
 		end
