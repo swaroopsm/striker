e6b5b9538a5c9b31147a13a8da4358ff4f1f1ff5
Swaroop SM <swaroop.striker@gmail.com>
2013-10-13 12:59:19 +0530

    added pages_tempate path in settings

--- lib/striker/settings.rb
+++ lib/striker/settings.rb
@@ -3,10 +3,13 @@ module Striker
 
 		# Default directory paths
 		SOURCE_DIR     = Dir.pwd
-		PAGES_DIR      = File.join(SOURCE_DIR, "pages")
-		TEMPLATES_DIR  = File.join(SOURCE_DIR, "templates")
-		MEDIA_DIR      = File.join(SOURCE_DIR, "media")
-		PUBLIC_DIR 		 = File.join(SOURCE_DIR, "public")
+		PAGES_DIR      = File.join(SOURCE_DIR, 'pages')
+		TEMPLATES_DIR  = File.join(SOURCE_DIR, 'templates')
+		MEDIA_DIR      = File.join(SOURCE_DIR, 'media')
+		PUBLIC_DIR 		 = File.join(SOURCE_DIR, 'public')
+
+		# Templates path for new page generation
+		PAGES_TEMPLATE  = File.expand_path(File.join('../command/templates'), __FILE__)
 
 	end
 end
