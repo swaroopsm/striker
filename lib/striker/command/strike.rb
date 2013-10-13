67693944fe207faf0b34e02c41cf600d7ac859d0
Swaroop SM <swaroop.striker@gmail.com>
2013-10-13 17:55:45 +0530

    run the server in run method of strike class

--- lib/striker/command/strike.rb
+++ lib/striker/command/strike.rb
@@ -5,7 +5,12 @@ module Striker
 		class Strike
 
 			def self.run(args, options)
-				
+				port = Settings::CONFIG['port']
+				root = Settings::PUBLIC_DIR
+				server = WEBrick::HTTPServer.new(:Port => port, :DocumentRoot => root)
+
+				trap 'INT' do server.shutdown end
+				server.start
 			end
 
 		end
