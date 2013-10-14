153ffe6ad904a493dc25d2c4416bfa66b96ba3be
Swaroop SM <swaroop.striker@gmail.com>
2013-10-14 16:05:28 +0530

    added template attr reader

--- lib/striker/page.rb
+++ lib/striker/page.rb
@@ -7,7 +7,7 @@ module Striker
 	class Page
 
 
-		attr_reader :meta, :content, :title, :name
+		attr_reader :meta, :content, :title, :name, :template
 
 		def initialize(page)
 			page = File.join Settings::PAGES_DIR, page
@@ -16,6 +16,7 @@ module Striker
 			extract_content = File.open(page, 'r').read.match(/^(?<headers>---\s*\n.*?\n?)^(---\s*$\n?)/m)
 			@content = extract_content.post_match
 			@name = @meta['title'].to_url
+			@template = @meta['template']
 		end
 
 	end
