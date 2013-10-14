2156687588082fdbc0474f82458b11ff1bdf9ebb
Swaroop SM <swaroop.striker@gmail.com>
2013-10-14 16:06:42 +0530

    added process of pages in process method of build command

--- lib/striker/command/build.rb
+++ lib/striker/command/build.rb
@@ -4,6 +4,12 @@ module Striker
 			
 			def self.process
 				FileUtils.mkdir_p(Settings::PUBLIC_DIR)
+				site = Site.new
+				site.pages(true).each do |p|
+					page = Striker::Page.new(p)
+					t = Template.new(page)
+					t.process
+				end
 			end
 
 		end
