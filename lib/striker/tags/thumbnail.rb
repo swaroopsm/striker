6c947593aecdcbf63a1982019789bab28c758970
Swaroop SM <swaroop.striker@gmail.com>
2013-10-18 18:22:31 +0530

    initial custom thumbnail tag

---
+++ lib/striker/tags/thumbnail.rb
@@ -0,0 +1,29 @@
+module Striker
+	module Tags
+		class Thumbnail < Liquid::Tag
+			
+			def initialize(tag, markup, tokens)
+				super
+				@markup = markup
+				@tokens = tokens
+				if m = markup.match(/(\d+)w\s(\d+)h\s?#?\[?([\w|\d|\-|\s]+)*\]?\s?\.?\[?([\w|\d|\-|\s]+)*\]?/)
+					@width = m[1]
+					@height = m[2]
+					@id = m[3] || ""
+					@class = m[4] || ""
+				end
+			end
+
+			def render(context)
+				image = Media::Image.new(nil, image_options = { :width => @width, :height => @height, :context => context['page'] })
+				image.thumbnailize
+				%Q{
+					<img src="/#{Settings::CONFIG['assets']}/images/#{context['page']['thumbnail']['src']}" alt="#{context['page']['meta']['title']}"  id="#{@id}" class="#{@class}">
+				}
+			end
+
+		end
+	end
+end
+
+Liquid::Template.register_tag('thumbnail', Striker::Tags::Thumbnail)
