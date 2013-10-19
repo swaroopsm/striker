708ff7a336aa925441d0e3b785609b14e1305ed5
Swaroop SM <swaroop.striker@gmail.com>
2013-10-19 13:20:09 +0530

    the correct to generate pages and edia directories

--- lib/striker/command/new.rb
+++ lib/striker/command/new.rb
@@ -7,11 +7,11 @@ module Striker
 				site_name = args.join
 				FileUtils.mkdir site_name
 				FileUtils.cp_r Dir.glob(File.expand_path('../../../new_site', __FILE__) + '/*'), File.join(Dir.pwd, site_name)
-				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "media")
-				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "pages")
-				FileUtils.mkdir_p File.join(Settings::MEDIA_DIR,  "images")
-				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "videos")
-				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "sounds")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, site_name, "media")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, site_name, "pages")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, site_name, "media/images")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, site_name, "media/videos")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, site_name, "mediasounds")
 			end
 
 		end
