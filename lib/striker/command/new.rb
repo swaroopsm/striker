42a0bcf71e54bb59918d2dfc569b1608aa9e3210
Swaroop SM <swaroop.striker@gmail.com>
2013-10-19 12:13:02 +0530

    create some directories that were missed

--- lib/striker/command/new.rb
+++ lib/striker/command/new.rb
@@ -7,6 +7,11 @@ module Striker
 				site_name = args.join
 				FileUtils.mkdir site_name
 				FileUtils.cp_r Dir.glob(File.expand_path('../../../new_site', __FILE__) + '/*'), File.join(Dir.pwd, site_name)
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "media")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "pages")
+				FileUtils.mkdir_p File.join(Settings::MEDIA_DIR,  "images")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "videos")
+				FileUtils.mkdir_p File.join(Settings::SOURCE_DIR, "sounds")
 			end
 
 		end
