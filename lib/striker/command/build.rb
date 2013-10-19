9593f1b573db0c7a2b97af0d383e0a5bcc1445f3
Swaroop SM <swaroop.striker@gmail.com>
2013-10-19 10:59:18 +0530

    copy all directories in include_asset path

--- lib/striker/command/build.rb
+++ lib/striker/command/build.rb
@@ -6,6 +6,10 @@ module Striker
 				FileUtils.rm_rf(File.join Settings::PUBLIC_DIR, ".")
 				FileUtils.mkdir_p(Settings::PUBLIC_DIR)
 				FileUtils.mkdir_p(Settings::ASSETS_DIR)
+				Settings::CONFIG['include_assets'].each do |d|
+					FileUtils.cp_r(File.join(Settings::SOURCE_DIR, d), Settings::ASSETS_DIR) if File.exists? d
+				end
+				FileUtils.cp_r(File.join(Settings::SOURCE_DIR, "css"), File.join(Settings::ASSETS_DIR))
 				Dir.glob(Settings::MEDIA_DIR + "/*").each do |d|
 					FileUtils.mkdir_p File.join(Settings::ASSETS_DIR, d.split("/")[-1]) if File.directory? d
 				end
