e88683c80f6cf06554e2ed5d14613136aee6b414
Swaroop SM <swaroop.striker@gmail.com>
2013-10-21 18:13:39 +0530

    defined page_data and added permalink reader in page class

--- lib/striker/page.rb
+++ lib/striker/page.rb
@@ -7,7 +7,7 @@ module Striker
 	class Page
 
 
-		attr_reader :meta, :content, :title, :name, :template, :base_dir
+		attr_reader :meta, :content, :title, :name, :template, :base_dir, :permalink
 
 		def initialize(page)
 			@base_dir = File.basename page, File.extname(page)
@@ -16,6 +16,7 @@ module Striker
 			@title = @meta['title']
 			@content = extract_content.post_match
 			@name = @meta['title'].to_url
+			@permalink = permalink_page
 			@template = @meta['template']
 
 			# @meta['images'] = self.image.all
@@ -23,6 +24,16 @@ module Striker
 			self.image.thumbnail
 		end
 		
+		def page_data
+			data = self.meta
+			data['thumbnail'] = self.image.thumbnail
+			data['name'] = self.name
+			data['base_dir'] = self.base_dir
+			data['images'] = self.image.all
+
+			data
+		end
+
 		def image
 			Media::Image.new(self)
 		end
@@ -30,6 +41,23 @@ module Striker
 		private
 		def extract_content
 			File.open(@page, 'r').read.match(/^(?<headers>---\s*\n.*?\n?)^(---\s*$\n?)/m)
+		end
+
+		def permalink_page
+			filename = case Settings::CONFIG['permalink']['style']
+									when :name
+										@base_dir
+									when :title
+										@name
+									else
+										@name
+									end
+			if Settings::CONFIG['permalink']['pretty']
+				FileUtils.mkdir_p(File.join(Settings::PUBLIC_DIR, filename))
+				filename + "/index.html"
+			else
+				filename + ".html"
+			end
 		end
 
 	end
