39d3c80572590443ef756a6943c2ffb4128b2858
Swaroop SM <swaroop.striker@gmail.com>
2013-10-23 20:49:47 +0530

    defined list_full function for tag class

--- lib/striker/tag.rb
+++ lib/striker/tag.rb
@@ -11,6 +11,21 @@ module Striker
 			tags.flatten.uniq!
 		end
 
+		def self.list_full
+			tags = []
+			pages = []
+			Dir.chdir(Settings::PAGES_DIR)
+			pages = Dir.glob("*[.md|.markdown]").map{ |page| Page.new(page) }
+			self.list.each do |tag|
+				tagged = []
+				pages.each do |page|
+					tagged << page.page_data if page.meta['tags'] and page.meta['tags'].include? tag
+				end
+				tags << { 'name' => tag, 'pages' => tagged }
+			end
+			tags
+		end
+
 		def self.process
 			self.list.each do |tag|
 				FileUtils.mkdir_p(File.join(Settings::PUBLIC_DIR, Settings::CONFIG['tagged']['style'], tag))
