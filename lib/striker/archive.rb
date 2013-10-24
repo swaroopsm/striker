0a351ebf1f3552ffad20c27e55e66c70ea163aa8
Swaroop SM <swaroop.striker@gmail.com>
2013-10-24 19:11:38 +0530

    group archived pages based on year or month

--- lib/striker/archive.rb
+++ lib/striker/archive.rb
@@ -8,9 +8,14 @@ module Striker
 			Dir.glob("*[.md|markdown]").each do |page|
 				grouped_pages << Page.new(page).page_data
 			end
-			grouped_pages = grouped_pages.group_by{ |page| page['date'] }
-			grouped_pages.each do |p|
-				pages << { 'date' => p[0], 'pages' => p[1] }
+			if Settings::CONFIG['archive']['period'] == :year
+				grouped_pages = grouped_pages.group_by{ |page| page['date'].strftime("%Y") }
+			else
+				grouped_pages = grouped_pages.group_by{ |page| [page['date'].strftime("%B"), page['date'].strftime("%Y")] }
+			end
+			grouped_pages.each do |p| 
+				date = p[0].class == Array ? p[0].join(" ") : p[0]
+				pages << { 'date' => date, 'pages' => p[1] }
 			end
 			pages
 		end
