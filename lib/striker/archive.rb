a43384aeb9e07b39b754970c62c2e3c7e24d594d
Swaroop SM <swaroop.striker@gmail.com>
2013-10-29 17:59:00 +0530

    call process_archive_dir in process function

--- lib/striker/archive.rb
+++ lib/striker/archive.rb
@@ -3,7 +3,7 @@ module Striker
 		
 		def self.process
 			FileUtils.mkdir_p(File.join(Settings::PUBLIC_DIR, Settings::CONFIG['archive']['style']))
-			list_full
+			process_archive_dir
 		end
 
 		def self.list_full
@@ -19,20 +19,20 @@ module Striker
 				grouped_pages = grouped_pages.group_by{ |page| [page['date'].strftime("%Y"), page['date'].strftime("%B")] }
 			end
 			grouped_pages.each do |p| 
-				process_archive_dir(p[0])
 				date = p[0].class == Array ? Date.new(p[0][0].to_i, Date::MONTHNAMES.index(p[0][1]), 1): p[0]
 				pages << { 'date' => date, 'pages' => p[1] }
 			end
 			pages
 		end
 
-		def self.process_archive_dir(archive)
-			Dir.chdir(File.join(Settings::PUBLIC_DIR, Settings::CONFIG['archive']['style']))
-			month_number = Date::MONTHNAMES.index(archive[1]).to_s
-			if archive.class == Array
-				FileUtils.mkdir_p(File.join(archive[0], month_number))
-			else
-				FileUtils.mkdir_p(archive.to_s)
+		def self.process_archive_dir
+			list_full.each do |archive|
+				Dir.chdir(File.join(Settings::PUBLIC_DIR, Settings::CONFIG['archive']['style']))
+				if archive['date'].class == Date
+					FileUtils.mkdir_p(File.join(archive['date'].year.to_s, archive['date'].month.to_s))
+				else
+					FileUtils.mkdir_p(archive['date'].to_s)
+				end
 			end
 		end
 
