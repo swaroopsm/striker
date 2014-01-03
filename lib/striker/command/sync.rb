a0f4678b88f43ea45173e092587662f93f5461b8
Swaroop SM <swaroop.striker@gmail.com>
2014-01-03 16:14:45 +0530

    specified alias command called upload for sync

--- lib/striker/command/sync.rb
+++ lib/striker/command/sync.rb
@@ -10,7 +10,7 @@ module Striker
 
 			def process
 				@remote_path = File.join("~", @remote["path"])
-				@output = `rsync -avh #{self.public_dir} #{@remote['username']}@#{@remote['host']}:#{@remote_path}`
+				@output = `rsync -avhP #{self.public_dir} #{@remote['username']}@#{@remote['host']}:#{@remote_path}`
 
 				print "\n" + "---"*10 + "\n"
 				@output.split("\n").reject{ |o| o.strip.empty?  }.each{ |o| print o + "\n" }
