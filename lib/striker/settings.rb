0a5cb0fed01984434a4a3fb9ac4d19662dfd284c
Swaroop SM <swaroop.striker@gmail.com>
2014-01-03 16:43:48 +0530

    removed commented code in settings

--- lib/striker/settings.rb
+++ lib/striker/settings.rb
@@ -32,32 +32,8 @@ module Striker
 
 		end
 
-		# Default directory paths
-		# SOURCE_DIR     = Dir.pwd
-		# PAGES_DIR      = File.join(SOURCE_DIR, 'pages')
-		# TEMPLATES_DIR  = File.join(SOURCE_DIR, 'templates')
-		# MEDIA_DIR      = File.join(SOURCE_DIR, 'media')
-		# PLUGINS_DIR		 = File.join(SOURCE_DIR, 'plugins')
-
-		# # Templates path for new page generation
+	  # Templates path for new page generation
 		PAGES_TEMPLATE  = File.expand_path(File.join('../command/templates'), __FILE__)
-
-		# # Default configuration from config.yml
-		# if File.exists? File.join SOURCE_DIR, 'config.yml'
-		# 	CONFIG = YAML.load_file(File.join(SOURCE_DIR, 'config.yml'))
-
-		# 	PUBLIC_DIR 		 = File.join(SOURCE_DIR, CONFIG['destination'])
-
-		# 	BASEPATH 			 = File.join(PUBLIC_DIR, CONFIG['basepath'])
-
-		# 	ASSETS_DIR	 	 = File.join(BASEPATH, CONFIG['assets'])
-
-		# 	BASEURL 			 = File.join(CONFIG['basepath'])
-
-		# 	if CONFIG['gallerize']
-		# 		GALLERY_DIR  = File.join(SOURCE_DIR, '_gallery')
-		# 	end
-		# end
 
 	end
 end
