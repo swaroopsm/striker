1e5255cb50cf1dbb4ce0d1c3fead3bcb6986293e
Swaroop SM <swaroop.striker@gmail.com>
2014-01-03 16:06:11 +0530

    sync functionality

---
+++ lib/striker/command/sync.rb
@@ -0,0 +1,22 @@
+module Striker
+	module Command
+		class Sync < Settings
+		
+			def initialize(source, options)
+				super(source)
+				@options = options
+				@remote = self.config['remote']
+			end
+
+			def process
+				@remote_path = File.join("~", @remote["path"])
+				@output = `rsync -avh #{self.public_dir} #{@remote['username']}@#{@remote['host']}:#{@remote_path}`
+
+				print "\n" + "---"*10 + "\n"
+				@output.split("\n").reject{ |o| o.strip.empty?  }.each{ |o| print o + "\n" }
+				print "---"*10 + "\n\n"
+			end
+
+		end
+	end
+end
