7fb04971af94936dcf82b2fc7d45d9b1c62848c5
Swaroop SM <swaroop.striker@gmail.com>
2014-01-15 18:51:30 +0530

    added robots.txt processing in sitemap class

--- lib/striker/command/sitemap.rb
+++ lib/striker/command/sitemap.rb
@@ -2,7 +2,8 @@ module Striker
 	module Command
 		class Sitemap < Base
 
-			FILENAME = "sitemap.xml"
+			SITEMAP_FILE = "sitemap.xml"
+			ROBOTS_FILE = "robots.txt"
 
 			def initialize(args, options, path)
 				super(args, options, path)
@@ -11,20 +12,27 @@ module Striker
 				@remote = @site.meta['server']['remote']
 			end
 
-			def generate
-				generate_sitemap
+			def process
+				generate
 			end
 
-			def liquidize
-				Liquid::Template.parse(sitemap_template).render(
-			  	'sitemap' => sitemap_data
-				)
+			def sitemap
+				liquidize(sitemap_template)
+			end
+
+			def robots
+				liquidize(robots_template)
 			end
 
 			private
+			def generate
+				generate_sitemap
+				generate_robots
+			end
+
 			def generate_sitemap
-				File.open(File.join(@site.settings.extras_dir, FILENAME), "w") do |f|
-					f.write(self.liquidize)
+				File.open(File.join(@site.settings.extras_dir, SITEMAP_FILE), "w") do |f|
+					f.write(self.sitemap)
 				end
 			end
 
@@ -50,8 +58,32 @@ module Striker
 				File.read(File.join(File.dirname(File.expand_path(__FILE__)), "templates/sitemap.xml"))
 			end
 
+			def robots_template
+				File.read(File.join(File.dirname(File.expand_path(__FILE__)), "templates/robots.txt"))
+			end
+			
+			def robots_data
+				{ 
+					'disallow' => File.join(@site.settings.baseurl, @site.settings.config['assets']),
+					'sitemap' => File.join(@site.settings.baseurl, SITEMAP_FILE) 
+				}
+			end
+
 			def parse_date(date)
 				date.strftime("%Y-%m-%d")
+			end
+
+			def generate_robots
+				File.open(File.join(@site.settings.extras_dir, ROBOTS_FILE), "w") do |f|
+					f.write(self.robots)
+				end
+			end
+
+			def liquidize(template)
+				Liquid::Template.parse(template).render(
+			  	'sitemap' => sitemap_data,
+			  	'robots' => robots_data
+				)
 			end
 
 		end
