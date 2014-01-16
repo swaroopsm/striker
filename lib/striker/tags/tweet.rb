fb93b2dda44114391d8ed01c1e56110f16476249
Swaroop SM <swaroop.striker@gmail.com>
2014-01-16 11:38:59 +0530

    removed mentions and added via for twitter tag

--- lib/striker/tags/tweet.rb
+++ lib/striker/tags/tweet.rb
@@ -5,14 +5,14 @@ module Striker
 			def initialize(tag, markup, tokens)
 				super
 				hashtags = markup.match /\s?#\[([\w\-~$^*!?.:;+=\s]+)\]/
-				mentions = markup.match /\s?@\[([\w\-~$^*!?.:;+=\s]+)\]/
+				via = markup.match /\s?@\[([\w\-~$^*!?.:;+=\s]+)\]/
 				@hashtags = hashtags[1].gsub(" ", ",") unless hashtags.nil?
-				@mentions = mentions[1].gsub(" ", ",") unless mentions.nil?
+				@via = via[1].gsub(" ", ",") unless via.nil?
 			end
 
 			def render(context)
 				<<-TWEET
-					<a href="https://twitter.com/share" class="twitter-share-button" data-via="smswaroop" data-hashtags="#{@hashtags}" data-related="#{@mentions}">Tweet</a>
+					<a href="https://twitter.com/share" class="twitter-share-button" data-via="#{@via}" data-hashtags="#{@hashtags}" data-related="#{@mentions}">Tweet</a>
 					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 				TWEET
 			end
